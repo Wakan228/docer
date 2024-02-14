65a914da86b2f3a95bf4ce207f96a0c1b67004f5
Константин <32998913+Wakan228@users.noreply.github.com>
2024-02-14 16:54:39 +0200

    v1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,56 +4,68 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product as Product;
+
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+     /**
+      * Create a new controller instance.
+      *
+      * @return void
+      */
+     // public function __construct()
+     // {
+     //     $this->middleware('auth');
+     // }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
-    }
+     /**
+      * Show the application dashboard.
+      *
+      * @return \Illuminate\Contracts\Support\Renderable
+      */
+     public function index()
+     {
+          return view('home');
+     }
 
-    static function aboutCompany(){
-        //return view('app/aboutCompany');
-        dd(Product::get_flow_by_auth());dd('null');
-   }
-   static function aboutWater(){
-        return view('app/aboutWater');
-   }
-   static function blog(){
-        return view('app/blog');
-   }
-   static function contacts(){
-        return view('app/contacts');
-   }
-   static function delivery(){
-        return view('app/delivery');
-   }
-   static function stock(){
-        return view('app/stock');
-   } function ssd (){echo 'like';}
-   static function store(){
-        return view('app/store');
-   }
-   static function cart(){
-        return view('app/cart');
-   }
-   static function login(){
-        return view('app/login');
-   }
-
-
+     static function aboutCompany()
+     {
+          //return view('app/aboutCompany');
+          dd(Product::get_flow_by_auth());
+     }
+     static function aboutWater()
+     {
+          return view('app/aboutWater');
+     }
+     static function blog()
+     {
+          return view('app/blog');
+     }
+     static function contacts()
+     {
+          return view('app/contacts');
+     }
+     static function delivery()
+     {
+          return view('app/delivery');
+     }
+     static function stock()
+     {
+          return view('app/stock');
+     }
+     function ssd()
+     {
+          echo 'like';
+     }
+     static function store()
+     {
+          return view('app/store');
+     }
+     static function cart()
+     {
+          return view('app/cart');
+     }
+     static function login()
+     {
+          return view('app/login');
+     }
 }
